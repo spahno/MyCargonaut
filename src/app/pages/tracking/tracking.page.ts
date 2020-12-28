7c7bf17196570739d29c83d41c4940e6b01d5343
Vorname Nachname“git config --global user.name Julian <julian.hermanspahn@mni.thm.de>
2020-12-29 00:18:49 +0100

    stable version

---
+++ src/app/pages/tracking/tracking.page.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-tracking',
+    templateUrl: './tracking.page.html',
+    styleUrls: ['./tracking.page.scss'],
+})
+export class TrackingPage {
+
+    constructor() {
+    }
+
+}
