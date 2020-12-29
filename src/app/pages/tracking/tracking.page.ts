a8ae864bda94021a5642e7f0beddfcf661211b29
Vorname Nachname“git config --global user.name Julian <julian.hermanspahn@mni.thm.de>
2020-12-29 01:45:50 +0100

    created tracking page

--- src/app/pages/tracking/tracking.page.ts
+++ src/app/pages/tracking/tracking.page.ts
@@ -1,13 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {Router} from '@angular/router';
+import {icon, Map, marker, tileLayer} from 'leaflet';
 
 @Component({
     selector: 'app-tracking',
     templateUrl: './tracking.page.html',
     styleUrls: ['./tracking.page.scss'],
 })
-export class TrackingPage {
+export class TrackingPage implements OnInit {
 
-    constructor() {
+    customMarkerIcon = icon({
+        iconUrl: '../assets/icon/marker.png',
+        iconSize: [64, 64],
+        popupAnchor: [0, -20]
+    });
+
+    constructor(public platform: Platform,
+                public router: Router) {
     }
 
+    initMap() {
+        const container = document.getElementById('map');
+        if (container) {
+            const map = new Map('map').setView([33.6396965, -84.4304574], 23);
+            tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            marker([33.6396965, -84.4304574], {icon: this.customMarkerIcon})
+                .bindPopup()
+                // .on('click', () => this.router.navigateByUrl('/tracking'))
+                .addTo(map);
+            // .openPopup();
+        }
+    }
+
+    ngOnInit() {
+        this.initMap();
+    }
 }
