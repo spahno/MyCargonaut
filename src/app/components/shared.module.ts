f22805585cdbfe6f827a8c98d07ab12328c9806b
Lewe Lorenzen <lewe.lorenzen@mni.thm.de>
2020-12-30 19:13:50 +0100

    Dark Mode auskommentiert

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import {AnfrageCardComponent} from './anfrage-card/anfrage-card.component';
+import {ProfilPopoverComponent} from './profil-popover/profil-popover.component';
 
 @NgModule({
-    declarations: [AnfrageCardComponent ],
+    declarations: [AnfrageCardComponent, ProfilPopoverComponent],
     imports: [CommonModule, IonicModule],
-    exports: [AnfrageCardComponent]
+    exports: [AnfrageCardComponent, ProfilPopoverComponent]
 })
 export class SharedModule {}
