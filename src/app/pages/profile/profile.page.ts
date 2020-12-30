951442f8a6f0edce8354848bbfc00720c81a1ba2
Vorname Nachname“git config --global user.name Julian <julian.hermanspahn@mni.thm.de>
2020-12-30 20:40:55 +0100

    profile page styled

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from '../../../services/auth/auth.service';
+import {User} from '../../../models/user';
+import {Fahrzeug} from '../../../models/fahrzeug';
+import {AuthService} from "../../../services/auth/auth.service";
 
 @Component({
     selector: 'app-profile',
@@ -8,10 +10,35 @@ import {AuthService} from '../../../services/auth/auth.service';
 })
 export class ProfilePage implements OnInit {
 
+    testUser: User;
+    car: Fahrzeug = {
+        nummernschild: 'LDKRL777',
+        marke: 'Audi',
+        modell: 'A4',
+        fahrzeugart: 'Limousine',
+        farbe: 'silber',
+        baujahr: 2020,
+        ladeflaeche: [{hoehe: 75, breite: 180, tiefe: 180}]
+    };
+    cars: Fahrzeug[] = [];
+
     constructor(public authService: AuthService) {
     }
 
     ngOnInit() {
+        this.testUser = {
+            id: 'lol',
+            username: 'max power',
+            vorname: 'max',
+            nachname: 'mustermann',
+            email: 'maxpower@mail.com',
+            bewertung: 0,
+            fahrzeuge: [],
+            angebote: [],
+            gesuche: [],
+            anfragen: []
+        };
+        this.cars.push(this.car);
     }
 
 }
