8d896d681f054f697a1932f17652fee20c5c76f5
Vorname Nachname“git config --global user.name Julian <julian.hermanspahn@mni.thm.de>
2020-12-30 15:50:09 +0100

    fixed auth functions

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -21,7 +21,7 @@ export class RegisterPage implements ViewDidEnter {
 
     constructor(private authService: AuthService,
                 private router: Router) {
-        if (authService.user) {
+        if (this.authService.loggedIn) {
             this.router.navigate(['/profile']);
         }
     }
