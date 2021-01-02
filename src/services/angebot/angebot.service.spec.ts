c70c6108cf64263dea959c5462297cc28e3f1dea
Lewe Lorenzen <lewe.lorenzen@mni.thm.de>
2021-01-02 15:29:08 +0100

    Wrote Test for angebot.service.spec.ts wrote Update delete for angebot.service.ts

--- src/services/angebot/angebot.service.spec.ts
+++ src/services/angebot/angebot.service.spec.ts
@@ -1,70 +1,124 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AngebotService } from './angebot.service';
+import {AngebotService} from './angebot.service';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../../environments/environment';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {Angebot} from '../../models/Angebot';
 
 describe('AngebotService', () => {
-  let service: AngebotService;
-  const angebot1: Angebot = new Angebot();
-  angebot1.ankunftDatum = '17.12.2020';
-  angebot1.ankunftZeit = '17:12 Uhr';
-  angebot1.abfahrtStrasse = 'Gießenerstraße 299';
-  angebot1.abfahrtPlz = '35390';
-  angebot1.abfahrtOrt = 'Gießen';
-  angebot1.ankunftStrasse = 'Berlinerstraße 399';
-  angebot1.ankunftPlz = '10201';
-  angebot1.ankunftOrt = 'Berlin';
-  angebot1.bezahlung = '30€ VB';
-  const angebot2: Angebot = new Angebot();
-  angebot2.ankunftDatum = '18.12.2020';
-  angebot2.ankunftZeit = '18:12 Uhr';
-  angebot2.abfahrtStrasse = 'Heidelbergerstraße 299';
-  angebot2.abfahrtPlz = '69120';
-  angebot2.abfahrtOrt = 'Heidelberg';
-  angebot2.ankunftStrasse = 'Konstanzerstraße 399';
-  angebot2.ankunftPlz = '80923';
-  angebot2.ankunftOrt = 'Konstanz';
-  angebot2.bezahlung = '30€ Pro 1m^3';
+    let service: AngebotService;
+    const addedAngebote: string[] = [];
+    const angebot1: Angebot = new Angebot();
+    angebot1.ankunftDatum = '17.12.2020';
+    angebot1.ankunftZeit = '17:12 Uhr';
+    angebot1.abfahrtStrasse = 'Gießenerstraße 299';
+    angebot1.abfahrtPlz = '35390';
+    angebot1.abfahrtOrt = 'Gießen';
+    angebot1.ankunftStrasse = 'Berlinerstraße 399';
+    angebot1.ankunftPlz = '10201';
+    angebot1.ankunftOrt = 'Berlin';
+    angebot1.bezahlung = '30€ VB';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        AngularFireModule.initializeApp(environment.testFirebaseConfig),
-        AngularFirestoreModule,
-      ]
+    const angebot2: Angebot = new Angebot();
+    angebot2.ankunftDatum = '18.12.2020';
+    angebot2.ankunftZeit = '18:12 Uhr';
+    angebot2.abfahrtStrasse = 'Heidelbergerstraße 299';
+    angebot2.abfahrtPlz = '69120';
+    angebot2.abfahrtOrt = 'Heidelberg';
+    angebot2.ankunftStrasse = 'Konstanzerstraße 399';
+    angebot2.ankunftPlz = '80923';
+    angebot2.ankunftOrt = 'Konstanz';
+    angebot2.bezahlung = '30€ Pro 1m^3';
+
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            imports: [
+                AngularFireModule.initializeApp(environment.testFirebaseConfig),
+                AngularFirestoreModule,
+            ]
+        });
+        service = TestBed.inject(AngebotService);
     });
-    service = TestBed.inject(AngebotService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
-  describe('Firebase', () => {
-    let testData;
-    let angeboteBefore: Angebot[];
-    let angeboteAfter: Angebot[];
-    it('CRUD Angebot on Firebase', async (done) => {
-      const subAngebote = await service.observableAngebote().subscribe(async data => {
-        angeboteBefore = data;
-        expect(angeboteBefore).toBeDefined();
-      });
-      subAngebote.unsubscribe();
-      await service.addAngebot(angebot1)
-          .then(res => {
-            testData = res.angebote;
-            expect(res.message).toBeDefined();
-          }).catch(err => {
-            console.log(err);
-          });
-      await service.observableAngebote().subscribe(async data => {
-        angeboteAfter = data;
-        expect(angeboteAfter.length).toBe(angeboteBefore.length + 1);
-      });
-      done();
+    afterAll(async () => {
+        addedAngebote.forEach(angebot => {
+            service.deleteAngebot(angebot).then(() => {
+                console.log('TestBed Successfully reset');
+            }).catch(err => {
+                console.log('Failed reset TestBed. Error: ' + err);
+            });
+        });
     });
-  });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    describe('Firebase', () => {
+        it('Should get from Firebase', async (done) => {
+            const subAngebote = await service.observableAngebote().subscribe(data => {
+                subAngebote.unsubscribe();
+                expect(data).toBeDefined();
+                done();
+            });
+        });
+
+        it('Should add angebote to Firebase', async (done) => {
+            let angeboteBefore: Angebot[];
+            let angeboteAfter: Angebot[];
+            let addedAngebot1: Angebot;
+            let addedAngebot2: Angebot;
+            const subAngebote = await service.observableAngebote().subscribe(async data => {
+                angeboteBefore = data;
+                expect(angeboteBefore).toBeDefined();
+            });
+            subAngebote.unsubscribe();
+            await service.addAngebot(angebot1)
+                .then(async res => {
+                    addedAngebot1 = res.angebot;
+                    expect(res.message).toBeDefined();
+                    expect(res.angebot.bezahlung).toBe(angebot1.bezahlung);
+                    addedAngebote.push(res.angebot._ID);
+                }).catch(err => {
+                    console.log(err);
+                });
+            await service.addAngebot(angebot2)
+                .then(async res => {
+                    addedAngebot2 = res.angebot;
+                    expect(res.message).toBeDefined();
+                    expect(res.angebot.bezahlung).toBe(angebot2.bezahlung);
+                    addedAngebote.push(res.angebot._ID);
+                }).catch(err => {
+                    console.log(err);
+                });
+            const sub2 = await service.observableAngebote().subscribe(data => {
+                sub2.unsubscribe();
+                angeboteAfter = data;
+                expect(angeboteAfter.length).toBe(angeboteBefore.length + 2);
+                expect(service.addAngebot).toHaveBeenCalledTimes(2);
+            });
+            done();
+        });
+
+
+        it('Test update Angebot on Firebase', (done) => {
+            let addedAngebot1 = new Angebot();
+            service.addAngebot(angebot1)
+                .then(res => {
+                    addedAngebot1 = res.angebot;
+                    expect(res.message).toBeDefined();
+                    expect(res.angebot.bezahlung).toBe(angebot1.bezahlung);
+                    addedAngebote.push(res.angebot._ID);
+                    addedAngebot1.bezahlung = 'Test €';
+                    service.updateAngebot(addedAngebot1).then(res2 => {
+                        expect(res2.angebot._ID).toEqual(addedAngebot1._ID);
+                        expect(res2.angebot.bezahlung).toEqual('Test €');
+                        done();
+                    });
+                }).catch(err => {
+                console.log(err);
+            });
+        });
+    });
 });
--- src/services/angebot/angebot.service.ts
+++ src/services/angebot/angebot.service.ts
@@ -9,13 +9,14 @@ import {IError} from 'protractor/built/exitCodes';
   providedIn: 'root'
 })
 export class AngebotService {
-  /***
-   * This service implements the CRUD of an Angebot.
-   */
-  angebotCollection: AngularFirestoreCollection<Angebot>;
-  constructor(private afs: AngularFirestore) {
-    this.angebotCollection = afs.collection<Angebot>('angebote');
-  }
+    /***
+     * This service implements the CRUD of an Angebot.
+     */
+    angebotCollection: AngularFirestoreCollection<Angebot>;
+
+    constructor(private afs: AngularFirestore) {
+        this.angebotCollection = afs.collection<Angebot>('angebote');
+    }
 
     /**
      * copy and prepare
@@ -43,44 +44,59 @@ export class AngebotService {
         return copy;
     }
 
-  /**
-   * This method creates a Observable of the Angebote from Firebase
-   */
-  observableAngebote(): Observable<Angebot[]> {
-    const changeActions: Observable<DocumentChangeAction<Angebot>[]> = this.angebotCollection.snapshotChanges();
-    return changeActions.pipe(
-        map(actions => actions.map(a => {
-          const data = a.payload.doc.data();
-          data._ID = a.payload.doc.id;
-          return data;
-        })));
-  }
+    /**
+     * This method creates a Observable of the Angebote from Firebase
+     * @return Observable<Angebot[]> of the Angebote in firebase
+     */
+    observableAngebote(): Observable<Angebot[]> {
+        const changeActions: Observable<DocumentChangeAction<Angebot>[]> = this.angebotCollection.snapshotChanges();
+        return changeActions.pipe(
+            map(actions => actions.map(a => {
+                const data = a.payload.doc.data();
+                data._ID = a.payload.doc.id;
+                return data;
+            })));
+    }
 
-  addAngebot(angebot: Angebot): Promise<{ angebote: Angebot[], message: string }> {
-      return new Promise((resolve, reject) => {
-          this.angebotCollection.add(AngebotService.copyAndPrepare(angebot))
-              .then((docRef) => {
-                  resolve({angebote: [new Angebot()], message: 'Document written with ID: ' + docRef.id});
-              }).catch((error) => {
-              reject('Error adding document: ' + error);
-          });
-      });
-  }
+    /**
+     * This Method adds a Angebot to firebase
+     * @param angebot is the Angebot that will be added
+     * @return Promise<{ angebot: Angebot, message: string } angebot is the added angebot, message Document written with ID: DocumentID
+     */
+    addAngebot(angebot: Angebot): Promise<{ angebot: Angebot, message: string }> {
+        return new Promise((resolve, reject) => {
+            this.angebotCollection.add(AngebotService.copyAndPrepare(angebot))
+                .then((docRef) => {
+                    const resAngebot = angebot;
+                    resAngebot._ID = docRef.id;
+                    resolve({angebot: resAngebot, message: 'Document written with ID: ' + docRef.id});
+                }).catch((error) => {
+                reject('Error adding document: ' + error);
+            });
+        });
+    }
 
-  deleteAngebot(angebot: Angebot): Promise<{ angebote: Angebot[], message: string }> {
-      return new Promise((resolve, reject) => {
-          this.angebotCollection.doc(angebot._ID).delete()
-              .then( async () => {
-                  let tmpAngebote: Angebot[];
-                  const sub = await this.observableAngebote().subscribe(data => {
-                      tmpAngebote = data;
-                  });
-                  sub.unsubscribe();
-                  resolve({angebote: tmpAngebote, message: 'Successfully deleted Document with ID: ' + angebot._ID});
-              }).catch((error) => {
-              reject('Error adding document: ' + error);
-          });
-      });
-  }
+    deleteAngebot(angebotId: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.angebotCollection.doc(angebotId).delete()
+                .then(async () => {
+                    resolve('Successfully deleted Document with ID: ' + angebotId);
+                }).catch((error) => {
+                reject('Error deleting document: ' + error);
+            });
+        });
+    }
 
+    updateAngebot(angebot: Angebot): Promise<{ angebot: Angebot, message: string }> {
+        return new Promise((resolve, reject) => {
+            const angebotId = angebot._ID;
+            this.angebotCollection.doc(angebot._ID).update(AngebotService.copyAndPrepare(angebot))
+                .then(async () => {
+                    angebot._ID = angebotId;
+                    resolve({angebot, message: 'Successfully edited Document with ID: ' + angebot._ID});
+                }).catch((error) => {
+                reject('Error editing document: ' + error);
+            });
+        });
+    }
 }
