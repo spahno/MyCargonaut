9e2ddb765df48438e3569258ea42e92bb433e037
Richard Leichner <richard.leichner@mni.thm.de>
2021-01-06 03:24:01 +0100

    minor

--- src/app/components/fahrzeugdetails/fahrzeugdetails.component.spec.ts
+++ src/app/components/fahrzeugdetails/fahrzeugdetails.component.spec.ts
@@ -1,40 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {IonicModule} from '@ionic/angular';
 
-import { FahrzeugdetailsComponent } from './fahrzeugdetails.component';
+import {FahrzeugdetailsComponent} from './fahrzeugdetails.component';
 import {Fahrzeug} from '../../../models/fahrzeug';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('FahrzeugdetailsComponent', () => {
-  let component: FahrzeugdetailsComponent;
-  let fixture: ComponentFixture<FahrzeugdetailsComponent>;
+    let component: FahrzeugdetailsComponent;
+    let fixture: ComponentFixture<FahrzeugdetailsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FahrzeugdetailsComponent ],
-      imports: [IonicModule.forRoot(), FormsModule, CommonModule]
-    }).compileComponents();
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [FahrzeugdetailsComponent],
+            imports: [AngularFireModule.initializeApp(environment.testFirebaseConfig),
+                AngularFirestoreModule, IonicModule.forRoot(),
+                FormsModule, CommonModule, RouterTestingModule]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(FahrzeugdetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+        fixture = TestBed.createComponent(FahrzeugdetailsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    }));
 
-  it('should create', () => {
-    component.fahrzeug  = {
-      nummernschild: '',
-      marke: '',
-      modell: '',
-      fahrzeugart: '',
-      farbe: '',
-      baujahr: 0,
-      hoehe: 0,
-      breite: 0,
-      tiefe: 0
-    };
-    component.detailmode = false;
-    component.editmode = false;
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        component.fahrzeug = {
+            nummernschild: '',
+            marke: '',
+            modell: '',
+            fahrzeugart: '',
+            farbe: '',
+            baujahr: 0,
+            hoehe: 0,
+            breite: 0,
+            tiefe: 0
+        };
+        component.detailmode = false;
+        component.editmode = false;
+        expect(component).toBeTruthy();
+    });
 });
